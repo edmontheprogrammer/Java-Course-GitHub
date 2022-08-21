171a6af481e574c80574c3e24ea62ca56a0428c2
Edmon the Programmer <edmontheprogrammer@gmail.com>
2022-08-21 08:08:40 +0800

    @edmontheprogrammer
    Debugging and Deploying Java Applications Commit

---
+++ Debugging_and_Deploying_Applications/Common_Syntax_Errors/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+         * 1. Data type declaration
+         * 2. Adding a semicolon at the end of the statments
+         * 3. Forgetting to add parenthesis on method call, name()
+         * 4. Forgetting to add quotes when priting strings
+         * 5. Misspelling variables, variable names
+         * 6. Using declaring variables that are already
+         * reserved keywords in Java, like 'class' and 'public'
+         * 7. Using single '=' equal sign to compare values instead of
+         * double equal signs to compare primitive data types.
+         * 8.
+         */
+        String name = "Bob";
+        System.out.println("Hello World");
+        int i = 0;
+        // if(i = 2)
+    }
+}
---
+++ Debugging_and_Deploying_Applications/Types_of_Errors/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+         * Types of Errors in Java
+         * 1. Compile Time Errors:
+         * Compile time Errors or syntax errors that prevent us from compiling our
+         * applications, these errors happen when we don't follow the grammar or
+         * syntax of Java language, that's why we call them syntax errors, the other
+         * errors don't happen at compile time, they happen at run time, when we run
+         * our applicaitons. These compile time errors are easy to find and fix.
+         * Our code editors like IntelliJ and VS Code tell us about the error.
+         * But when you encouter an error that doesn't really, make sense, your best
+         * source is Google and stackoverflow website.
+         * 2. Runtime Errors:
+         * Runtime errors are errors that happen at run time, when we run our
+         * applicaitons. Run time errors are little tricky, to find run time errors we
+         * need to use a tool called Debugger. So we want to run our application using
+         * this debugger, and with this we'll execute our code line by line and look at
+         * the value of various variables, and this will help us identify the source
+         * of the problem.
+         * 
+         * 
+         */
+
+    }
+}

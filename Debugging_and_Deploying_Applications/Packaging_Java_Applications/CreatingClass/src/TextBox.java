a857a8e0a111026a49e225731382ee8cda9b20ca
Edmon the Programmer <edmontheprogrammer@gmail.com>
2022-08-21 19:00:17 +0800

    @edmontheprogrammer
    Debugging and Deploying Applications Commits

---
+++ Debugging_and_Deploying_Applications/Packaging_Java_Applications/CreatingClass/src/TextBox.java
@@ -0,0 +1,12 @@
+public class TextBox {
+    public String text = ""; // Field
+
+    public void setText(String text) {
+       this.text = text;
+    }
+
+    public void clear() {
+        text = "";
+    }
+
+}
